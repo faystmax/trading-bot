7d6cf7191e43b387118515dd60ac0ac4ef5d48dd
Amosov Maxim <faystmax@gmail.com>
2020-05-03 12:04:51 +0700

    Move project to Junit 5

--- api/src/test/java/com/faystmax/tradingbot/BaseTest.java
+++ api/src/test/java/com/faystmax/tradingbot/BaseTest.java
@@ -1,12 +1,9 @@
 package com.faystmax.tradingbot;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.telegram.telegrambots.starter.TelegramBotInitializer;
 
-@RunWith(SpringRunner.class)
 @MockBean(TelegramBotInitializer.class)
 @SpringBootTest(classes = TradingBotApplication.class)
 public abstract class BaseTest {
--- api/src/test/java/com/faystmax/tradingbot/impl/BinanceServiceTest.java
+++ api/src/test/java/com/faystmax/tradingbot/impl/BinanceServiceTest.java
@@ -2,16 +2,16 @@ package com.faystmax.tradingbot.impl;
 
 import com.binance.api.client.BinanceApiRestClient;
 import com.binance.api.client.domain.account.Account;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Not actually a test
  * Just methods for checking binance Api
  */
-@Ignore
+@Disabled
 public class BinanceServiceTest {
     private BinanceApiRestClient restClient;
 
