c96d067e4109de02da7d71a81df132f9c68f4a78
Amosov Maxim <faystmax@gmail.com>
2020-05-03 15:26:55 +0700

    Add TradingBotApplicationStartUpTest for checking full app startup

---
+++ api/src/test/java/com/faystmax/tradingbot/TradingBotApplicationStartUpTest.java
@@ -0,0 +1,20 @@
+package com.faystmax.tradingbot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.telegram.telegrambots.starter.TelegramBotInitializer;
+
+@ExtendWith(SpringExtension.class)
+@MockBean(TelegramBotInitializer.class)
+@SpringBootTest(classes = TradingBotApplication.class)
+@ActiveProfiles("test")
+public class TradingBotApplicationStartUpTest {
+    @Test
+    public void checkStartUpSuccess() {
+        System.out.println("TradingBotApplication starts successfully!");
+    }
+}
