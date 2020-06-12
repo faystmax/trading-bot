3cdae5d48547ee3e5d03e3123e034a2d8096ae6d
Amosov Maxim <faystmax@gmail.com>
2020-06-12 17:25:57 +0700

    Fix SignInPage.test

--- ui/src/App/SignInPage/SignInPage.test.js
+++ ui/src/App/SignInPage/SignInPage.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import SignInPage from './SignInPage';
 
-test('renders learn react link', () => {
+test('renders sign in button', () => {
   const { getByText } = render(<SignInPage />);
-  const linkElement = getByText(/Trading Bot/i);
-  expect(linkElement).toBeInTheDocument();
+  const signInButton = getByText(/Sign In/);
+  expect(signInButton).toBeInTheDocument();
 });
