7aaff2d29458312dc251e4ca191e71b1a33214f8
Amosov Maxim <faystmax@gmail.com>
2020-06-14 21:12:01 +0700

    Add custom UserDetailsImpl and UserDetailsServiceImpl

---
+++ api/src/main/java/com/faystmax/tradingbot/service/user/UserDetailsImpl.java
@@ -0,0 +1,47 @@
+package com.faystmax.tradingbot.service.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 2741034895223838537L;
+
+    private Long id;
+    private String email;
+    @JsonIgnore
+    private String password;
+    private Boolean enabled;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
---
+++ api/src/main/java/com/faystmax/tradingbot/service/user/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.faystmax.tradingbot.service.user;
+
+import com.faystmax.tradingbot.db.entity.User;
+import com.faystmax.tradingbot.db.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static java.util.Collections.singleton;
+
+@Service
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepo userRepo;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
+
+        return new UserDetailsImpl(
+            user.getId(),
+            user.getEmail(),
+            user.getPassword(),
+            user.getEnabled(),
+            singleton(new SimpleGrantedAuthority(user.getRole().name())));
+    }
+}
