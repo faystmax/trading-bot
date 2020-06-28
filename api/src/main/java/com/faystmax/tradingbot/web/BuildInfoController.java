6253a5dfe3c594a23000747d95fd72404b9389f4
Amosov Maxim <faystmax@gmail.com>
2020-06-28 18:35:22 +0700

    Change Map to LinkedHashMap in BuildInfoController

--- api/src/main/java/com/faystmax/tradingbot/web/BuildInfoController.java
+++ api/src/main/java/com/faystmax/tradingbot/web/BuildInfoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @RestController
@@ -15,12 +16,12 @@ public class BuildInfoController {
 
     @Autowired
     public BuildInfoController(BuildProperties buildProperties) {
-        buildInfo = Map.of(
-            "name", buildProperties.getName(),
-            "group", buildProperties.getGroup(),
-            "version", buildProperties.getVersion(),
-            "time", DateUtils.format(Date.from(buildProperties.getTime()))
-        );
+        buildInfo = new LinkedHashMap<>() {{
+            put("name", buildProperties.getName());
+            put("group", buildProperties.getGroup());
+            put("version", buildProperties.getVersion());
+            put("time", DateUtils.format(Date.from(buildProperties.getTime())));
+        }};
     }
 
     @GetMapping(value = {"/", "version"})
