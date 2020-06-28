6ce527fcee9f3c489b53ac5fd5a6a0fd482844c4
Amosov Maxim <faystmax@gmail.com>
2020-06-28 15:26:02 +0700

    Add @Nullable to proxy

--- api/src/main/java/com/faystmax/tradingbot/config/TelegramConfig.java
+++ api/src/main/java/com/faystmax/tradingbot/config/TelegramConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
+import javax.annotation.Nullable;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -19,6 +20,7 @@ public class TelegramConfig {
     private String botName;
     @NotNull
     private Long chatId;
+    @Nullable
     private Proxy proxy;
 
     @Data
