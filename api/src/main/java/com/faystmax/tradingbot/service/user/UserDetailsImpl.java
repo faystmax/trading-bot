3d469dfaa037473f5005fd7822a1b6d8eee00e73
Amosov Maxim <faystmax@gmail.com>
2020-06-28 18:16:34 +0700

    Fix UserDetailsImpl

--- api/src/main/java/com/faystmax/tradingbot/service/user/UserDetailsImpl.java
+++ api/src/main/java/com/faystmax/tradingbot/service/user/UserDetailsImpl.java
@@ -27,17 +27,17 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
