ec8197e69f382acfd5856850f6ac165ab0917fc2
Amosov Maxim <faystmax@gmail.com>
2020-07-12 17:21:25 +0700

    Fix sign in test

--- ui/src/App/SignInPage/SignInPage.test.js
+++ ui/src/App/SignInPage/SignInPage.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { AuthContext } from '../../utils/auth';
 import SignInPage from './SignInPage';
 
 test('renders sign in button', () => {
-  const { getByText } = render(<SignInPage />);
+  const { getByText } = render(
+    <AuthContext.Provider value={{ auth: null }}>
+      <SignInPage />
+    </AuthContext.Provider>,
+  );
   const signInButton = getByText(/Sign In/);
   expect(signInButton).toBeInTheDocument();
 });
