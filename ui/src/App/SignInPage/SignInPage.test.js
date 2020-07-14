fe275a9662ef99c82576fd7ef672af819b286b5e
Amosov Maxim <faystmax@gmail.com>
2020-07-14 21:36:49 +0700

    Fix SignInPage.test.js

--- ui/src/App/SignInPage/SignInPage.test.js
+++ ui/src/App/SignInPage/SignInPage.test.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { AuthContext } from '../../utils/auth';
 import SignInPage from './SignInPage';
 
 test('renders sign in button', () => {
-  const { getByText } = render(
-    <AuthContext.Provider value={{ auth: null }}>
-      <SignInPage />
-    </AuthContext.Provider>,
-  );
+  const { getByText } = render(<SignInPage />);
   const signInButton = getByText(/Sign In/);
   expect(signInButton).toBeInTheDocument();
 });
--- ui/src/utils/auth.js
+++ ui/src/utils/auth.js
@@ -1,6 +1,6 @@
 import { createContext, useContext } from 'react';
 
-export const AuthContext = createContext();
+export const AuthContext = createContext({});
 
 export function useAuth() {
   return useContext(AuthContext);
