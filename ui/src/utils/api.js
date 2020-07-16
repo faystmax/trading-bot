9f9c3c786d309156cf56ab1eba72a083e2c41386
Amosov Maxim <faystmax@gmail.com>
2020-07-16 21:08:09 +0700

    Move baseURL to env variable

--- ui/src/utils/api.js
+++ ui/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8090/',
+  baseURL: process.env.REACT_APP_BASE_URL,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
