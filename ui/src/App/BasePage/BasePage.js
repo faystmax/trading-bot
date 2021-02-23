523ef0a7cf56c582900c940ca35b29d5181d298b
Amosov Maxim <faystmax@gmail.com>
2021-02-23 17:28:21 +0700

    Add mobile menu

--- ui/src/App/BasePage/BasePage.js
+++ ui/src/App/BasePage/BasePage.js
@@ -1,4 +1,5 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
+import clsx from 'clsx';
 import {
   AppBar,
   Drawer,
@@ -14,6 +15,8 @@ import { ExitToApp as ExitToAppIcon } from '@material-ui/icons';
 import BookIcon from '@material-ui/icons/Book';
 import PersonIcon from '@material-ui/icons/Person';
 import { useHistory } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import { useAuth } from 'utils/auth';
 import useStyles from './styles';
 
@@ -21,15 +24,38 @@ const BasePage = ({ children }) => {
   const classes = useStyles();
   const history = useHistory();
   const { auth, setAuth } = useAuth();
+  const [open, setOpen] = useState(false);
 
   const logOut = useCallback(() => {
     setAuth(null);
   }, [setAuth]);
 
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" className={classes.appBar}>
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
         <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden,
+            )}
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" noWrap className={classes.title}>
             Trading-bot
           </Typography>
@@ -42,40 +68,43 @@ const BasePage = ({ children }) => {
         </Toolbar>
       </AppBar>
       <Drawer
-        className={classes.drawer}
         variant="permanent"
         classes={{
-          paper: classes.drawerPaper,
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
         }}
+        open={open}
       >
-        <Toolbar />
-        <div className={classes.drawerContainer}>
+        {' '}
+        <div className={classes.toolbarIcon}>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <div>
           <List>
             <ListItem
               button
               key="My Orders"
+              className={classes.menuListItem}
+              onClick={() => history.push('/')}
               selected={window.location.pathname === '/'}
             >
               <ListItemIcon>
                 <BookIcon />
               </ListItemIcon>
-              <ListItemText
-                primary="My Orders"
-                onClick={() => history.push('/')}
-              />
+              <ListItemText primary="My Orders" />
             </ListItem>
             <ListItem
               button
               key="Profile"
+              className={classes.menuListItem}
+              onClick={() => history.push('/profile')}
               selected={window.location.pathname === '/profile'}
             >
               <ListItemIcon>
                 <PersonIcon />
               </ListItemIcon>
-              <ListItemText
-                primary="Profile"
-                onClick={() => history.push('/profile')}
-              />
+              <ListItemText primary="Profile" />
             </ListItem>
           </List>
         </div>
--- ui/src/App/BasePage/styles.js
+++ ui/src/App/BasePage/styles.js
@@ -7,24 +7,69 @@ const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
   },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...mainTheme.mixins.toolbar,
+  },
   appBar: {
     zIndex: mainTheme.zIndex.drawer + 1,
+    transition: mainTheme.transitions.create(['width', 'margin'], {
+      easing: mainTheme.transitions.easing.sharp,
+      duration: mainTheme.transitions.duration.leavingScreen,
+    }),
   },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: mainTheme.transitions.create(['width', 'margin'], {
+      easing: mainTheme.transitions.easing.sharp,
+      duration: mainTheme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
   },
   drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
     width: drawerWidth,
+    transition: mainTheme.transitions.create('width', {
+      easing: mainTheme.transitions.easing.sharp,
+      duration: mainTheme.transitions.duration.enteringScreen,
+    }),
   },
-  drawerContainer: {
-    overflow: 'auto',
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: mainTheme.transitions.create('width', {
+      easing: mainTheme.transitions.easing.sharp,
+      duration: mainTheme.transitions.duration.leavingScreen,
+    }),
+    width: mainTheme.spacing(7),
+    [mainTheme.breakpoints.up('sm')]: {
+      width: mainTheme.spacing(9),
+    },
   },
   title: {
     flexGrow: 1,
   },
+  menuListItem: {
+    [mainTheme.breakpoints.up('sm')]: {
+      paddingLeft: mainTheme.spacing(3),
+    },
+  },
   content: {
     flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
     padding: mainTheme.spacing(3),
   },
 }));
