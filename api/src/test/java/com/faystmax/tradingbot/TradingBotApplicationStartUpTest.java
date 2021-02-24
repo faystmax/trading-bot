e9c8396ea57990b2e85c600b20ccc230e55e8264
Amosov Maxim <faystmax@gmail.com>
2021-02-24 23:11:05 +0700

    Mock MailIdleServiceImpl in startUpTest

--- api/src/test/java/com/faystmax/tradingbot/TradingBotApplicationStartUpTest.java
+++ api/src/test/java/com/faystmax/tradingbot/TradingBotApplicationStartUpTest.java
@@ -1,5 +1,6 @@
 package com.faystmax.tradingbot;
 
+import com.faystmax.tradingbot.service.mail.impl.MailIdleServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ import org.telegram.telegrambots.starter.TelegramBotInitializer;
 
 @ExtendWith(SpringExtension.class)
 @MockBean(TelegramBotInitializer.class)
+@MockBean(MailIdleServiceImpl.class)
 @SpringBootTest(classes = TradingBotApplication.class)
 @ActiveProfiles("test")
 public class TradingBotApplicationStartUpTest {
