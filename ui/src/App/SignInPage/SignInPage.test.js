82f4fbdbe3e492fec5a277b8909fefb01e9b0636
Amosov Maxim <faystmax@gmail.com>
2021-03-15 14:52:24 +0700

    Fix ui tests

--- ui/src/App/SignInPage/SignInPage.test.js
+++ ui/src/App/SignInPage/SignInPage.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import SignInPage from './SignInPage';
 
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: () => jest.fn(),
+}));
+
 test('renders sign in button', () => {
   const { getByText } = render(<SignInPage />);
   const signInButton = getByText(/Sign In/);
--- ui/src/App/SignUpPage/SignUpPage.test.js
+++ ui/src/App/SignUpPage/SignUpPage.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import SignUpPage from './SignUpPage';
 
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: () => jest.fn(),
+}));
+
 test('renders sign up button', () => {
   const { getByText } = render(<SignUpPage />);
   const signUpButton = getByText(/Sign up new user/);
