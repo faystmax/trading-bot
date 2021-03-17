0b4467b3ebb20dfaeea5984485e802b6ae06c841
Amosov Maxim <faystmax@gmail.com>
2021-03-17 12:47:16 +0700

    Add checkEmailFields

--- api/src/main/java/com/faystmax/tradingbot/service/mail/impl/MailIdleStartUpService.java
+++ api/src/main/java/com/faystmax/tradingbot/service/mail/impl/MailIdleStartUpService.java
@@ -5,6 +5,7 @@ import com.faystmax.tradingbot.db.repo.UserRepo;
 import com.faystmax.tradingbot.service.mail.MailIdleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -27,13 +28,32 @@ public class MailIdleStartUpService implements ApplicationListener<ContextRefres
         int mailIdleCount = 0;
         for (User user : users) {
             try {
-                mailIdleService.createIdle(user);
-                mailIdleService.startIdle(user);
-                mailIdleCount++;
+                if (checkEmailFields(user)) {
+                    mailIdleService.createIdle(user);
+                    mailIdleService.startIdle(user);
+                    mailIdleCount++;
+                }
             } catch (Exception ex) {
                 log.error("Can't start mail Idle for user = " + user, ex);
             }
         }
         log.info("Ending mailIdle! mailIdleCount = " + mailIdleCount);
+    }
+
+    private boolean checkEmailFields(User user) {
+        if (StringUtils.isEmpty(user.getEmailHost())) {
+            log.warn("EmailHost is empty! user = {}", user.getEmail());
+            return false;
+        } else if (StringUtils.isEmpty(user.getEmailUsername())) {
+            log.warn("EmailUsername is empty! user = {}", user.getEmail());
+            return false;
+        } else if (StringUtils.isEmpty(user.getEmailPassword())) {
+            log.warn("EmailPassword is empty! user = {}", user.getEmail());
+            return false;
+        } else if (StringUtils.isEmpty(user.getEmailFolder())) {
+            log.warn("EmailFolder is empty! user = {}", user.getEmail());
+            return false;
+        }
+        return true;
     }
 }
