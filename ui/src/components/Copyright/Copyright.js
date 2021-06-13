429fa9a1d15ee27cf3d2ea2176eba0f6c1f15b4e
Amosov Maxim <mamosov@it-one.ru>
2021-06-13 12:40:02 +0700

    Update URl

--- ui/src/components/Copyright/Copyright.js
+++ ui/src/components/Copyright/Copyright.js
@@ -6,7 +6,7 @@ const Copyright = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://faystmax.pro/">
+      <Link color="inherit" href="https://faystmax.club/">
         Trading Bot
       </Link>{' '}
       {new Date().getFullYear()}
