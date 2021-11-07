4f3243397bf8238e385d0f745a2a8a5bd0559aec
Amosov Maxim <mamosov@it-one.ru>
2021-11-07 12:22:30 +0700

    Increase jwtExpirationMs

--- api/src/main/java/com/faystmax/tradingbot/config/security/jwt/JwtUtils.java
+++ api/src/main/java/com/faystmax/tradingbot/config/security/jwt/JwtUtils.java
@@ -21,7 +21,7 @@ public class JwtUtils {
     private String jwtSecret;
 
     @Value("${jwt.expiration}")
-    private int jwtExpirationMs;
+    private long jwtExpirationMs;
 
     public String generateJwtToken(final Authentication authentication) {
         final UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
