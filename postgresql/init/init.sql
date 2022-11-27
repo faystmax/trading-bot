79e299442cc32bdfebff63bfb5235287789c40b0
Amosov Maxim <faystmax@gmail.com>
2022-11-27 20:33:11 +0300

    Update docker-compose.yml

--- postgresql/init/init.sql
+++ postgresql/init/init.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE trading_bot;
 CREATE USER trading_bot WITH ENCRYPTED PASSWORD 'password';
 GRANT ALL PRIVILEGES ON DATABASE trading_bot TO trading_bot;
+GRANT ALL PRIVILEGES ON SCHEMA public TO trading_bot;
